40f88ee10135928c09319eb9dc5eea33a28e2dd6
Novi <alnoviantiramadhani91@gmail.com>
2024-02-03 17:18:16 +0700

    push gcf

---
+++ gudang/function.go
@@ -0,0 +1,39 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/warehousemanagement88/be_warehouse/module"
+)
+
+func init() {
+	functions.HTTP("warehouse", warehouse_gudang)
+}
+
+func warehouse_gudang(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://stocksynergy.my.id")
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// if r.Method == http.MethodPost {
+	// 	fmt.Fprintf(w, module.GCFHandlerInsertGudangA("PASETOPUBLICKEY", "MONGOSTRING", "warehouse_db", r))
+	// 	return
+	// }
+	// if r.Method == http.MethodPut {
+	// 	fmt.Fprintf(w, module.GCFHandlerUpdateGudangA("PASETOPUBLICKEY", "MONGOSTRING", "warehouse_db", r))
+	// 	return
+	// }
+	// if r.Method == http.MethodDelete {
+	// 	fmt.Fprintf(w, module.GCFHandlerDeleteGudangA("PASETOPUBLICKEY", "MONGOSTRING", "warehouse_db", r))
+	// 	return
+	// }
+	// Set CORS headers for the main request.
+	fmt.Fprintf(w, module.GCFHandlerGetAllProducts("MONGOSTRING", "warehouse_db"))
+}
---
+++ webhook/function.go
@@ -0,0 +1,11 @@
+package gcf
+
+import (
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/whatsauth/webhook"
+)
+
+func init() {
+	functions.HTTP("WebHook", adaptor.FiberHandlerFunc(webhook.PostMessage))
+}
